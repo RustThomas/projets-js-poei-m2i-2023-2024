bf24a4bd614fb984a42e7d58a8d2bd9500624d46
Jessica da Rosa <jess_GRosa@hotmail.com>
2024-01-12 12:57:20 +0100

    ajout de la conjugaison au present

--- jessica-3-Conjugaison/script.js
+++ jessica-3-Conjugaison/script.js
@@ -1,3 +1,24 @@
 function conjuguer(){
+    var verbe = document.querySelector('textarea')
+    var conjugaisons = document.querySelector('#conjugaisons')
+
+    var present = conjuguerPresent(verbe.value)
+    if(present != null){
+        var p = document.createElement('p')
+        p.innerHTML = "Present: " + present
+        conjugaisons.appendChild(p)
+        verbe.value = ''
+    }
     
+
+}
+function conjuguerPresent(verbe){
+    if(verbe.endsWith("er")){
+        var garder = verbe.slice(0, -2);
+        var conjugue = garder + "e";
+        return conjugue;
+    }else{
+        alert("Désolé, cette fonction ne prend en charge que les verbes du premier groupe.");
+        return null
+    }
 }
