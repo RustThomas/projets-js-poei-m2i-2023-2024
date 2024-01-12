bad0b7984df320078826f40ca5a6940b71102647
Nicolas Duquesne <nicolas.duquesne@caramail.fr>
2024-01-12 15:03:54 +0100

    feature: operationnal calculations

--- nicolas-calculatrice/calc.js
+++ nicolas-calculatrice/calc.js
@@ -2,7 +2,11 @@
 let operatorButtons = document.querySelectorAll(".operator");
 let numberButtons = document.querySelectorAll(".number");
 let displayer = document.querySelector("#displayer");
+let clearButton = document.querySelector("#clear");
+let equalsButton = document.querySelector(".equal-sign");
 let numExpression = "" 
+
+
 //Attribution d'évènements
 
 operatorButtons.forEach(nb => {
@@ -13,12 +17,27 @@ numberButtons.forEach(nb => {
     nb.addEventListener('click', (event) => onClickEv(event))
 })
 
+clearButton.addEventListener('click', clearDisplayer);
+equalsButton.addEventListener('click', onEqualsClick)
+
+
+//Fonctions
 
 function onClickEv(event) {
     const eventValue = event.currentTarget.value
     const exp = buildNumExpression(eventValue)
     if(validsOnlyNum(exp)) {
         display(exp);
+    }
+}
+
+function onEqualsClick() {
+    console.log(numExpression);
+    if(validsOperationExpression(numExpression)) {
+        const res = eval(numExpression);
+        clearDisplayer();
+        display(res);
+        numExpression = "";
     }
 }
 
@@ -31,7 +50,16 @@ function display(expression) {
     displayer.value = expression;
 }
 
+function clearDisplayer() {
+    displayer.value="";
+}
+
 function validsOnlyNum(expression) {
     const pattern = /^[0-9]+\.?[0-9]*$/
     return pattern.test(expression);
+}
+
+function validsOperationExpression(expression) {
+    const pattern = /^[0-9]+\.?[0-9]*[\* || \/ || \- || +]{1}[0-9]+\.?[0-9]*$/
+    return pattern.test(expression)
 }
